445053ffd68520f02635bc96992206d0e45f8b00
lxpzdylib41-web <lxpzdylib41@gmail.com>
2026-08-14 03:28:38 -0700

    Update cookiesxacecodetyper.lua

--- cookiesxacecodetyper.lua
+++ cookiesxacecodetyper.lua
@@ -1961,7 +1961,7 @@ BrandImage.Size =
 BrandImage.BackgroundTransparency =
     1
 BrandImage.Image =   
-  "rbxassetid://101579838151121"
+  "rbxassetid://130803572607369"
 
 BrandImage.ScaleType =
     Enum.ScaleType.Fit
